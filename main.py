c1a0f9e4a682873e33b62dd5c1593f240bbb2f06
Tibor Dudlák <tdudlak@redhat.com>
2019-04-12 12:17:16 +0200

    Parameters parse

--- main.py
+++ main.py
@@ -9,21 +9,44 @@ Authors:    Martin Kopec <xkopec42@gmail.com>
 
 import argparse
 
+PRODUCT = "-p"
+BACKGROUND = "-b"
+TEXT = "-t"
+ANIMATION = "-a"
+FONT = "-f"
+STICKER = "-s"
+PLATFORM = "-P"
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--products", required=True,
+    parser.add_argument(PRODUCT, "--product",
+                        default=["data/pictures/02.jpeg"],
                         help="Product images")
-    parser.add_argument("-b", "--background", required=True,
-                        help="Background")
-    parser.add_argument("-t", "--text", required=True,
-                        help="Text to display")
+    parser.add_argument(BACKGROUND, "--background",
+                        default="data/videos/4KRGBcolors.mp4",
+                        required=False, help="Background")
+    parser.add_argument(TEXT, "--text",
+                        default="Hello World!",
+                        required=False, help="Text to display")
+    parser.add_argument(ANIMATION, "--animation",
+                        default=None,
+                        required=False, help="Animation")
+    parser.add_argument(FONT, "--font",
+                        default="data/fonts/",
+                        required=False, help="Font to use")
+    parser.add_argument(STICKER, "--sticker",
+                        default="data/stickers/doge.png",
+                        required=False, help="Font to use")
+    parser.add_argument(PLATFORM, "--platform",
+                        default=None,
+                        required=False, help="Target platform")
 
     return parser.parse_args()
 
 
 if __name__ == '__main__':
-    args = parse_args()
-    print("Hello world!")
-    for arg in args:
-        print(arg)
+    args = vars(parse_args())
+    print("Hello world!\n")
+    for key in args.keys():
+        print(key, ":", args[key])
